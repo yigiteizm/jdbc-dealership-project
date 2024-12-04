65a8eada2fa9929f60ce6ada8a4a3ecc7ebfe94c
Yigit <yigistaff@gmail.com>
2024-12-04 16:03:23 -0500

    searchByColor method has been added to VehicleDao class

--- src/main/java/com/yearup/dealership/db/VehicleDao.java
+++ src/main/java/com/yearup/dealership/db/VehicleDao.java
@@ -114,11 +114,25 @@ public class VehicleDao {
             return vehicles;
         }
 
-    
+
 
     public List<Vehicle> searchByColor(String color) {
-        // TODO: Implement the logic to search vehicles by color
-        return new ArrayList<>();
+        List<Vehicle> vehicles = new ArrayList<>();
+        String sql = "SELECT * FROM vehicles WHERE color = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, color);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    vehicles.add(createVehicleFromResultSet(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return vehicles;
     }
 
     public List<Vehicle> searchByMileageRange(int minMileage, int maxMileage) {
