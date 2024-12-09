ddb8d06f6d347d933e69f2fd1e8ebb6f34911d51
Yigit <yigistaff@gmail.com>
2024-12-08 23:45:16 -0500

    addVehicleToInventory method has been added to InventoryDao

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -13,8 +13,23 @@ public class InventoryDao {
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
-    }
+
+            String sql = "INSERT INTO inventory (dealership_id, VIN) VALUES (?, ?)";
+
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(sql)) {
+                // Dealership ID ve VIN değerlerini ayarla
+                statement.setInt(1, dealershipId);
+                statement.setString(2, vin);
+
+                // SQL sorgusunu çalıştır
+                statement.executeUpdate();
+                System.out.println("Vehicle with VIN " + vin + " added to dealership ID " + dealershipId);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
 
     public void removeVehicleFromInventory(String vin) {
         // TODO: Implement the logic to remove a vehicle from the inventory
